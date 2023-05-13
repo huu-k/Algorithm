6c70202a076ceda175d17a306660c35b906ee664
huu-k <125865310+huu-k@users.noreply.github.com>
2023-05-13 13:39:31 +0900

    [Silver II] Title: 최대 힙, Time: 124 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,34 @@
+'''
+첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 
+다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 
+만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, 
+x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
+입력되는 자연수는 231보다 작다.
+
+출력
+입력에서 0이 주어진 회수만큼 답을 출력한다. 
+만약 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력하면 된다.'''
+
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+for _ in range(n):
+    x = int(input())
+    
+    if x != 0 :
+        heapq.heappush(q, -x)
+    else:
+        if len(q) == 0:
+            print('0')
+        else:
+            print(-heapq.heappop(q))
+    
+
+
+
+
+
