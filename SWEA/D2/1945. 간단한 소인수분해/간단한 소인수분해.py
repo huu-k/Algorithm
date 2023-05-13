2c708d62eaadaf0cf53eedc0cae4a4e3a786e9d3
huu-k <125865310+huu-k@users.noreply.github.com>
2023-05-13 18:16:08 +0900

    [D2] Title: 간단한 소인수분해, Time: 158 ms, Memory: 58,240 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -6,28 +6,18 @@ N이 주어질 때 a, b, c, d, e 를 출력하라.
 
 test_case= int(input())
 
-for i in range(test_case):
+for j in range(test_case):
     count= [0]*5
+    num = [2,3,5,7,11]
     n = int(input())
     while n != 1:
-        if (n%2 == 0):
-        	count[0]+=1
-        	n = n//2
-        if (n%3 == 0):
-        	count[1]+=1
-        	n = n//3
-        if (n%5 == 0):
-        	count[2]+=1
-        	n = n//5
-        if (n%7 == 0):
-        	count[3]+=1
-        	n = n//7        
-        if (n%11 == 0):
-        	count[4]+=1
-        	n = n//11   
+        for i in range(5):
+            if n%num[i]==0:
+                count[i]+=1
+                n//=num[i]
             
 	
-    print('#'+str(i+1), end=' ')
+    print('#'+str(j+1), end=' ')
     for x in count:
     	print(x, end=' ')
         
