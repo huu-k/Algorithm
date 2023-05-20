5ab76c482d6f27bf4f573e4bf8d6eab9e463056c
huu-k <125865310+huu-k@users.noreply.github.com>
2023-05-20 22:19:25 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 97 ms, Memory: 41,920 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,12 +1,4 @@
-def recur(n, k):
-    if k<=0:
-        return 1
-    return n*recur(n,k-1)
-
-for test in range(10):
-    t = int(input())
-    n, k = map(int, input().split())
-
-    result = recur(n, k)
-
-    print('#' + str(t), result)
+for _ in range(10):
+    test = int(input()) 
+    n , k = map(int,input().split())
+    print('#'+str(test), n**k)
